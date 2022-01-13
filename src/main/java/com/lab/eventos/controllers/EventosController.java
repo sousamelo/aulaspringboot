fcd4e343b27b42470d66ea185eba5f939b23dd47
Rogerio Melo <rogeriomelo@outlook.com>
2022-01-13 16:08:02 -0300

    Enviando dados do formulário para a aplicação

--- src/main/java/com/lab/eventos/controllers/EventosController.java
+++ src/main/java/com/lab/eventos/controllers/EventosController.java
@@ -1,7 +1,10 @@
 package com.lab.eventos.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.lab.eventos.models.Evento;
 
 @Controller
 public class EventosController {
@@ -9,6 +12,12 @@ public class EventosController {
 	@RequestMapping("eventos/form")
 	public String form() {
 		return "formEvento";
+	}
+	
+	@PostMapping("/eventos")
+	public String adicionar(Evento evento) {
+		System.out.println(evento);
+		return "evento-adicionado";
 	}
 
 }
