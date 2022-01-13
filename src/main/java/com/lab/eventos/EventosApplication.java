3c2ba3d89718c44a2e7457fe1468e7091f561a69
Rogerio Melo <rogeriomelo@outlook.com>
2022-01-13 14:10:41 -0300

    Criação inicial do projeto

---
+++ src/main/java/com/lab/eventos/EventosApplication.java
@@ -0,0 +1,13 @@
+package com.lab.eventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventosApplication.class, args);
+	}
+
+}
